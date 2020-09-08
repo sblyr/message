aeb97c8f7aed5740909bf90d5ac225256fd9048e
Olivier Andriessen <olivier@andev.nl>
2020-09-08 20:36:42 +0000

    fix: 🐛 typo :)

--- services/message-client/src/Layout.js
+++ services/message-client/src/Layout.js
@@ -59,7 +59,7 @@ class Layout extends React.Component {
                             imageFitTypeId={'cover'}
                             imageUrl={window._env_.REACT_APP_BRAND_IMAGE_URL}
                             title={window._env_.REACT_APP_TITLE}
-                            description={process._env_.REACT_APP_VERSION}
+                            description={window._env_.REACT_APP_VERSION}
                             />
                         <MenuContainer>
                             <Menu>
